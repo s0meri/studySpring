51c626a880c9579a0c977b033e2393dd3df952c7
soeun823 <bsoeun866@gmail.com>
2025-11-20 14:34:30 +0900

    feat :: 로그인/ 회원가입 구현

---
+++ src/main/java/com/example/projectshop/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.example.projectshop;
+
+import com.example.projectshop.mamber.Member;
+import com.example.projectshop.mamber.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return User.withUsername(member.getUsername())
+                .password(member.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
---
+++ src/main/java/com/example/projectshop/mamber/MemberController.java
@@ -0,0 +1,39 @@
+package com.example.projectshop.mamber;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String register() {
+        return "register.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @PostMapping("/member")
+    public String addMember(
+            String username,
+            String password,
+            String displayName
+    ) {
+        Member member = new Member();
+        member.setUsername(username);
+        member.setPassword(passwordEncoder.encode(password));
+        member.setDisplayName(displayName);
+        memberRepository.save(member);
+
+        return "redirect:/list";
+    }
+}
